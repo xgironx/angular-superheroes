7999b7bb092c0d23d4bdc0def363c8b0d49d1bab
Mike Nabil <maikel_nabil@yahoo.com>
2017-03-06 16:28:26 -0500

    buttons show and hide images

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ angular
   .module("heroesApp", [])
   .controller('heroesCtrl', [heroesController])
 
-function heroesController () {
+function heroesController (){
   this.heroes = superheroes
 
 }
